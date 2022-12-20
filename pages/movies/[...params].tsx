3e6088cf4a78cd9799a7234a4548f7833bb078b4
hozunlee <ho2yahh@gmail.com>
2022-12-20 13:41:36 +0900

    getServerSideProps params 관리

--- pages/movies/[...params].tsx
+++ pages/movies/[...params].tsx
@@ -1,10 +1,10 @@
 import Seo from 'components/Seo';
 import { useRouter } from 'next/router';
 
-const Detail = () => {
+const Detail = ({ params }) => {
   const router = useRouter();
   console.log('router :>> ', router);
-  const [title, id] = router.query.params ?? [];
+  const [title, id] = router.query.params ?? params;
   return (
     <div>
       <Seo title={title} />
@@ -15,3 +15,12 @@ const Detail = () => {
 };
 
 export default Detail;
+
+export function getServerSideProps({
+  params: { params },
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log('params :>> ', params);
+  return {
+    props: { params },
+  };
+}
